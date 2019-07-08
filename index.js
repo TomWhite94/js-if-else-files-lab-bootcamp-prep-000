f874b7bfa509a972a0fdd221c21559c872136863
Tom White <tomjameswhite94@gmail.com>
2019-07-08 23:28:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var name = "Joe"
+var name = "susan"
 var height = "74"
 
-
+if (name === "susan") {
+  console.log("The name is incorrect")
+} else {
+  console.log("Expected the name to be susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
